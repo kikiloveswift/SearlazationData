bc758c117add3d4a393744e364adcffc64d6010a
kingleekong <konglee873@gmail.com>
2016-01-16 15:54:19 +0800

    解析数据
    解析字典型字符串数据

--- SearlazationData/main.m
+++ SearlazationData/main.m
@@ -10,8 +10,22 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//        NSString *jsonString = @"{\"ID\":{\"Content\":268,\"type\":\"text\"},\"ContractTemplateID\":{\"Content\":65,\"type\":\"text\"}}";
+        NSString *jsonString = @"{\"index\":1,\"url\":\"api35/Product/GetGroupProductPriceInfo\",\"param\":\"{\\\"ProductID\\\":\\\"81457\\\",\\\"GroupID\\\":\\\"1293251\\\",\\\"MemberId\\\":-1}\",\"usecache\":1}";
+        NSData *data = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+        id json = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+        NSLog(@"json is %@",json);
+        NSString *jsonStr = [json objectForKey:@"param"];
+        
+        NSData *dataJson = [jsonStr dataUsingEncoding:NSUTF8StringEncoding];
+        id result = [NSJSONSerialization JSONObjectWithData:dataJson options:0 error:nil];
+        NSLog(@"result is %@",result);
+        for (NSString *key in result) {
+            NSString *value = [result objectForKey:key];
+            NSLog(@"%@-------%@",key,value);
+        }
+        
     }
     return 0;
 }
+
